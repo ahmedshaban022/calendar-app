25df4bd0e9408cfcfe49d25e802d1692f0d3fae8
ahmedshaban022 <ahmedshaban.ua007@gmail.com>
2023-09-29 12:00:57 +0300

    remove unused code on Calendar component

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -4,13 +4,10 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin!
 import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction"; // needed for dayClick
 import timeGridPlugin from "@fullcalendar/timegrid";
-
-import { EventClickArg } from "@fullcalendar/core/index.js";
 import { useEvents } from "@/context/EventsContext";
 import EventForm from "./EventForm";
 import CustomDialog from "./Dialog";
-import { event } from "@/types/calenderTypes";
-import EventCard from "./EventCard";
+
 import DayEvents from "./DayEvents";
 
 interface CalendarProps {}
@@ -18,33 +15,15 @@ interface CalendarProps {}
 const Calendar: FC<CalendarProps> = ({}) => {
   const [selectedDay, setSelectedDay] = useState<Date | null>(null);
   const [openDialog, setOpenDialog] = useState(false);
-  const [dayEvents, setDayEvents] = useState<event[]>([]);
-  const {
-    events,
-    setEvents,
-    createEvent,
-    getEventsByDay,
-    deleteEvent,
-    editEvent,
-    getEventDetails,
-  } = useEvents();
+
+  const { events } = useEvents();
 
   const handleDateClick = (arg: DateClickArg) => {
     setOpenDialog(true);
-    // setDayEvents(getEventsByDay(arg.date));
     setSelectedDay(arg.date);
   };
-  const eventActions = (arg: EventClickArg) => {
-    console.log("Event !! clicked");
-    // deleteEvent(arg.event.id);
-    const event = getEventDetails(arg.event.id);
-    if (!event) {
-      return null;
-    }
-    event.title = "Hello";
-    editEvent(event);
-  };
-  useEffect(() => {}, [events, dayEvents]);
+
+  useEffect(() => {}, [events]);
   return (
     <main className="   ">
       <div className="text-right">
@@ -77,7 +56,7 @@ const Calendar: FC<CalendarProps> = ({}) => {
           center: "title",
           right: "dayGridMonth,timeGridWeek,timeGridDay",
         }}
-        eventClick={eventActions}
+        // eventClick={eventActions}
       />
     </main>
   );
