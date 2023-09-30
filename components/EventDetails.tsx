442b9c0318726c6afc5750c54ff2991a506f931a
ahmedshaban022 <ahmedshaban.ua007@gmail.com>
2023-09-30 19:31:07 +0300

    replace the two icons with SVG and uninstall lucide-react

--- components/EventDetails.tsx
+++ components/EventDetails.tsx
@@ -6,7 +6,6 @@ import Priority from "@/components/Priority";
 import { useEvents } from "@/context/EventsContext";
 import { formatDate } from "@/lib/utils";
 import { event } from "@/types/calenderTypes";
-import { CalendarClock } from "lucide-react";
 import Link from "next/link";
 // import { useParams } from "next/navigation";
 import { FC, useEffect, useState } from "react";
@@ -94,7 +93,25 @@ const EventDetails: FC<EventDetailsProps> = ({ id }) => {
               </div>
             </div>
             <div className="mb-2 mt-10 flex  justify-center font-semibold text-2xl border-b-2 border-black ">
-              <CalendarClock />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                className="lucide lucide-calendar-clock"
+              >
+                <path d="M21 7.5V6a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h3.5" />
+                <path d="M16 2v4" />
+                <path d="M8 2v4" />
+                <path d="M3 10h5" />
+                <path d="M17.5 17.5 16 16.25V14" />
+                <path d="M22 16a6 6 0 1 1-12 0 6 6 0 0 1 12 0Z" />
+              </svg>
               <p> {formatDate(event.date)}</p>
             </div>
             {event?.start && event?.end ? (
